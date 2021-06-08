90780a3fb5355db8e48e9c63a6fe8c1e29f65775
danainjax <danainjax@gmail.com>
2021-06-08 11:23:55 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// let totalBatteries = batteryBatches.reduce(function(sum, batts){
+//     return batts + sum
+// });
+
+const totalBatteries = batteryBatches.reduce(
+    (accumulator, element) => accumulator + element
+     
+);
+
+const snacks = ["pizza", "stuffed mushrooms", "tacos", "hot pockets"];
+
+const feast = snacks.reduce(
+    (accumulator, snack) => accumulator + " * " + snack
+);
+
+feast
+//not understanding this, working on it...ok taking the array batteryBatches, and calling the array method .reduce on it which takes
+//a callback function, in this case the word function (which is a function delcaration )
+//that takes 4 possible arguments, in this case we provide two- the accumulator (what we are adding up- can be called anything we want)
+//and the current value
